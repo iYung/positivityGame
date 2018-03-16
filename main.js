65646613445d50cd55fdd3a259175d59be14870f
iYung <ivankhyung@gmail.com>
2018-03-16 12:14:40 -0400

    analyzes typed sentences in reddit replies

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 const SentimentIntensityAnalyzer = require('vader-sentiment');
-var intensity = SentimentIntensityAnalyzer.polarity_scores(input);
+const input = 'VADER is very smart, handsome, and funny';
+const intensity = SentimentIntensityAnalyzer.polarity_scores(input);
+console.log(intensity);
+
+var sentence = "";
+
+ document.addEventListener('click',function(e){
+    console.log(sentence);
+    if(e.target && e.target.innerHTML == 'save'){
+        console.log(SentimentIntensityAnalyzer.polarity_scores(sentence));
+    }
+})
+
+ document.addEventListener('keyup',function(e){
+    if (e.target.localName == 'textarea') {
+        sentence = e.target.value;
+    };
+})
