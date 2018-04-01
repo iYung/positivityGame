458e56f42372545d5456bc983c5eb62602c17e80
iYung <ivankhyung@gmail.com>
2018-04-01 00:41:52 -0400

    built update function in the back end

--- extension/background.js
+++ extension/background.js
@@ -1,12 +1,21 @@
-//sets up score 
-var score = localStorage.getItem("postivityGame_positivityScore");
-var id = localStorage.getItem("postivityGame_userID");
-
 //uploads score every x
 setInterval(function()
 {
+    var score = localStorage.getItem("postivityGame_positivityScore");
     //upload score
     if (score) {
-        
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", 'https://positivity-game-iyung.c9users.io/api/update', true);
+
+        //Send the proper header information along with the request
+        xhr.setRequestHeader("Content-type", "application/json");
+
+        xhr.onreadystatechange = function() {//Call a function when the state changes.
+            if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                // Request finished. Do processing here.
+                console.log("Uploaded score!");
+            }
+        }
+        xhr.send({ score: score });
     }
 },30000);
